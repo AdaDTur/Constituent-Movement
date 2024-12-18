559257d3da846399ecfea26848edd109f59107ab
Ada Tur <47090586+AdaDTur@users.noreply.github.com>
2024-12-18 18:56:30 -0500

    Update data_analysis.py

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -3,53 +3,38 @@ import numpy as np
 import statsmodels.api as sm
 from patsy import dmatrices
 
-model_names = ["gpt2", "gpt2_med", "gpt2_large", "gpt2_xl", "distilgpt2", "llama2", "llama2_chat", "rwkv"]
+model_names = ["gpt2", "gpt2_med", "gpt2_large", "gpt2_xl", "llama_3", "llama_3_chat", "mistral_0.3", "mistral_0.3_chat", "babyllama", "babyopt", "olmo", "olmo_chat"]
+filepath = "MPP_new_olmo_chat.csv"
+df = pd.read_csv(filepath)
+
 for model_name in model_names:
-    print("============" + model_name.upper() + "============")
-    filepath = "data/data_file_" + model_name + "_final.json"
-
     #DIFFERENCE ANALYSIS
-    df = pd.read_json(filepath, lines=True)
-    df['adj_weight'] = df['adjectives'].apply(lambda x: len(x))
-    df['prep_weight'] = df['prepositions'].apply(lambda x: len(x))
+    df['adj1'] = df['adj1'].apply(lambda x: x if isinstance(x, list) else eval(x))
+    df['adj2'] = df['adj2'].apply(lambda x: x if isinstance(x, list) else eval(x))
+    df['preps1'] = df['preps1'].apply(lambda x: x if isinstance(x, list) else eval(x))
+    df['preps2'] = df['preps2'].apply(lambda x: x if isinstance(x, list) else eval(x))
+    df['adj_weight'] = df['adj1'].apply(lambda x: len(x)) + df['adj2'].apply(lambda x: len(x))
+    df['prep_weight'] = df['preps1'].apply(lambda x: len(x)) + df['preps2'].apply(lambda x: len(x))
     df['weight'] = df['adj_weight'] + df['prep_weight']
-    df_shifted = df[df['shifted']].reset_index(drop=True).rename(columns={'sentence': 'sentence_shifted'})
-    df_unshifted = df[~df['shifted']].reset_index(drop=True).rename(columns={'sentence': 'sentence_unshifted'})
-    df_merged = df_shifted.drop(columns=['shifted', 'score'])
-    df_merged['diff'] = df_unshifted['score'] - df_shifted['score']
-    for i in np.unique(df_merged['weight']):
-        weight_specific = df_merged[df_merged['weight'] == i]
-        print(f"At weight {i}, average unshifted-shifted: {np.mean(weight_specific['diff'])}")
+    df['diff'] = df[f'{model_name}_scores'].apply(lambda x: eval(x)[0] - eval(x)[1])
 
-    for i in np.unique(df_merged['adj_weight']):
-        weight_specific = df_merged[df_merged['adj_weight'] == i]
-        print(f"At adj weight {i}, average unshifted-shifted: {np.mean(weight_specific['diff'])}")
+    for i in np.unique(df['weight']):
+        weight_specific = df[df['weight'] == i]
+        #print(f"At weight {i}, average unshifted-shifted: {np.mean(weight_specific['diff'])}")
 
-    for i in np.unique(df_merged['prep_weight']):
-        weight_specific = df_merged[df_merged['prep_weight'] == i]
-        print(f"At prep weight {i}, average unshifted-shifted: {np.mean(weight_specific['diff'])}")
+    for i in np.unique(df['adj_weight']):
+        weight_specific = df[df['adj_weight'] == i]
+        #print(f"At adj weight {i}, average unshifted-shifted: {np.mean(weight_specific['diff'])}")
+
+    for i in np.unique(df['prep_weight']):
+        weight_specific = df[df['prep_weight'] == i]
+        #print(f"At prep weight {i}, average unshifted-shifted: {np.mean(weight_specific['diff'])}")
 
 
 
     # """
     #OLS REGRESSION
-    df = pd.read_json(filepath, lines=True)
-
-    for column in ['subject', 'verb', 'noun', 'adjectives', 'prepositions', 'final_con', 'final_con_type']:
-        #print(f"Variable: {column}\nUnique Values: {np.unique(df[column])}\n\n")
-        continue
-
-    df['adj_weight'] = df['adjectives'].apply(lambda x: len(x))
-    df['prep_weight'] = df['prepositions'].apply(lambda x: len(x))
-    df['weight'] = df['adj_weight']+df['prep_weight']
-
-
-    df_shifted = df[df['shifted']].reset_index(drop=True).rename(columns={'sentence':'sentence_shifted'})
-    df_unshifted = df[~df['shifted']].reset_index(drop=True).rename(columns={'sentence':'sentence_unshifted'})
-    df_merged = df_shifted.drop(columns=['shifted', 'score'])
-    df_merged['diff'] = df_unshifted['score'] - df_shifted['score']
-
-    y, X = dmatrices('diff ~ adj_weight + prep_weight', data=df_merged, return_type='dataframe') # This prepares your data in the right format for the statsmodels package
+    y, X = dmatrices('diff ~ adj_weight + prep_weight', data=df, return_type='dataframe') # This prepares your data in the right format for the statsmodels package
     '''
     Note that the string 'diff ~ adj_weight + prep_weight' follows what is often standard notation for regression modelling:
     the thing to the left of the '~' is your outcome variable, and the things to the right are the variables whose effect on the outcome variable you're evaluating.
@@ -58,7 +43,7 @@ for model_name in model_names:
 
     model = sm.OLS(y, X) # This sets up the linear regression
     res = model.fit() # This fits the linear regression model
+    print(model_name)
     print(res.summary()) # Results
     # """
     print("===================================")
-
