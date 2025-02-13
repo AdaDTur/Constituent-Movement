b8ad6564917246d0fc0cd9b3df54230bc5635338
Ada Tur <47090586+AdaDTur@users.noreply.github.com>
2025-02-13 10:26:02 -0500

    Update data_creation.py

--- code/data_creation.py
+++ code/data_creation.py
@@ -48,18 +48,40 @@ for subject in subjects:
 							dict_list.append(shifted_data)
 							count += 1
 
-with open('OUTPUT.json', 'w') as fp:
+with open('hnps_OUTPUT.json', 'w') as fp:
 	for dictionary in dict_list:
 		line = json.dumps(dictionary) + '\n'
 		fp.write(line)
 
 #====================GENERATING PM DATA====================
 
-prepositions1 = ["with the coat", "from California"]
-prepositions2 = ["in the store", "from the market", "at the park", "behind the gym", "at work", "at the event"]
-verbs = ["met", "saw", "passed", "greeted"]
-subjects = ["I", "He", "They", "She"]
-adjectives = ["wealthy", "tall", "chatty", "older"]
+prepositions1 = []
+prepositions2 = []
+pivots = {['gave', 'up']: {'the idea', 'her location', 'his secret', 'the race'}, 
+	  ['put', 'out']: [], 
+	  ['worked', 'out']: [], 
+	  ['turned', 'in']: [], 
+	  ['backs', 'up']: [], 
+	  ['brought', 'up']: [], 
+	  ['pointed', 'out']: [], 
+	  ['carries', 'out']: [], 
+	  ['called', 'off']: [], 
+	  ['hand', 'in']: [], 
+	  ['left', 'out']: [], 
+	  ['turned', 'down']: [], 
+	  ['set', 'up']: [], 
+	  ['threw', 'away']: [], 
+	  ['takes', 'apart']: [], 
+	  ['looks', 'up']: [], 
+	  ['beats', 'up']: [], 
+	  ['put', 'on']: [],
+	  ['cleans', 'up']: [], 
+	  ['kept', 'up']: [], 
+	  ['brought', 'in']: [],
+	  ['gave', 'away']: [], 
+	  ['called', 'back']: []}
+nouns = []
+adjectives = []
 
 count = 0
 dict_list = []
@@ -74,18 +96,18 @@ for subject in subjects:
 						prep_list = list(np.random.choice(prepositions1, weight2, replace=False))
 						if weight1 == 0:
 							if weight2 == 0:
-								unshifted_sentence = f"{subject} {verb} {noun} {prep2}."
-								shifted_sentence = f"{subject} {verb} {prep2} {noun}."
+								unshifted_sentence = f"John {verb} {noun} {prep2}."
+								shifted_sentence = f"John {verb} {prep2} {noun}."
 							else:
-								unshifted_sentence = f"{subject} {verb} {noun} {' '.join(prep_list)} {prep2}."
-								shifted_sentence = f"{subject} {verb} {prep2} {noun} {' '.join(prep_list)}."
+								unshifted_sentence = f"John {verb} {noun} {' '.join(prep_list)} {prep2}."
+								shifted_sentence = f"John {verb} {prep2} {noun} {' '.join(prep_list)}."
 						else:
 							if weight2 == 0:
-								unshifted_sentence = f"{subject} {verb} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]}{prep2}."
-								shifted_sentence = f"{subject} {verb} {prep2} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]}."
+								unshifted_sentence = f"John {verb} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]}{prep2}."
+								shifted_sentence = f"John {verb} {prep2} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]}."
 							else:
-								unshifted_sentence = f"{subject} {verb} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]} {' '.join(prep_list)} {prep2}."
-								shifted_sentence = f"{subject} {verb} {prep2} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]} {' '.join(prep_list)}."
+								unshifted_sentence = f"John {verb} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]} {' '.join(prep_list)} {prep2}."
+								shifted_sentence = f"John {verb} {prep2} {noun_split[0]} {', '.join(adj_list)} {noun_split[1]} {' '.join(prep_list)}."
 						data = [unshifted_sentence, shifted_sentence]
 
 						print(count)
@@ -97,7 +119,7 @@ for subject in subjects:
 						dict_list.append(shifted_data)
 						count += 1
 
-with open('OUTPUT.json', 'w') as fp:
+with open('pm_OUTPUT.json', 'w') as fp:
 	for dictionary in dict_list:
 		line = json.dumps(dictionary) + '\n'
 		fp.write(line)
@@ -149,7 +171,7 @@ for subject in subjects:
 							dict_list.append(shifted_data)
 							count += 1
 
-with open('OUTPUT.json', 'w') as fp:
+with open('da_OUTPUT.json', 'w') as fp:
 	for dictionary in dict_list:
 		line = json.dumps(dictionary) + '\n'
 		fp.write(line)
@@ -201,7 +223,7 @@ for subject in subjects:
 							dict_list.append(shifted_data)
 							count += 1
 
-with open('OUTPUT.json', 'w') as fp:
+with open('mpp_OUTPUT.json', 'w') as fp:
 	for dictionary in dict_list:
 		line = json.dumps(dictionary) + '\n'
 		fp.write(line)
