7e8ad2e625f2833861a41c3dc18af8a98cd1af10
Ada Tur <47090586+AdaDTur@users.noreply.github.com>
2025-02-13 11:59:22 -0500

    Update data_mean_scoring.py

--- code/data_mean_scoring.py
+++ code/data_mean_scoring.py
@@ -9,7 +9,6 @@ auth_token = os.getenv(mytoken)
 
 models = {'gpt2_large':'gpt2-large', 'gpt2_xl':'gpt2-xl', 'llama_3':'meta-llama/Meta-Llama-3-8B', 'llama_3_chat':'meta-llama/Meta-Llama-3-8B-Instruct', 'mistral_0.3':'mistralai/Mistral-7B-v0.3', 'mistral_0.3_chat':'mistralai/Mistral-7B-Instruct-v0.3', 'babyllama':'babylm/babyllama-100m-2024', 'babyopt':'babylm/opt-125m-strict-2023', 'olmo':'allenai/OLMo-7B-0724-hf', 'olmo_chat':'allenai/OLMo-7B-0724-Instruct-hf'} 
 
-### RUNNING MEAN REDUCTION FIRST
 for m_nick in models:
     mpp_data = pd.read_json('old_mpp_data.json', lines=True)
     mpp_out_file = f'mpp_{m_nick}_mean_scores.json'
